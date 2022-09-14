3c134286a380f2339e2819d3128032247f5bf3fe
johnkevs15 <john_kevin.campanilla@student.passerellesnumeriques.org>
2022-09-14 08:43:09 +0800

    php file exercise 7

---
+++ php_exer7_campanilla.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+        <center>
+        <form action="" method="post"  style=" width:300px; margin-top:100px; display: inline-block;">
+                <label >Quiz<input type="text" name="Quiz" required style ="width: 100%; padding: 12px 20px; margin: 8px 0;  border: 1px solid #ccc; box-sizing: border-box;"></label>
+                <label >Projects<input type="text" name="Projects" required style ="width: 100%; padding: 12px 20px; margin: 8px 0; border: 1px solid #ccc; box-sizing: border-box;"></label>
+                <label >Midterm Exam<input type="text" name="Midterm_Exam" required style ="width: 100%; padding: 12px 20px; margin: 8px 0;  border: 1px solid #ccc; box-sizing: border-box;"></label>
+                <label >Exercises<input type="text" name="Exercises" required style ="width: 100%; padding: 12px 20px; margin: 8px 0;  border: 1px solid #ccc; box-sizing: border-box;"></label>
+                <button type="submit" name="submit" style= " background-color: #04AA6D; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 100%;">Submit</button>
+        
+        
+        <?php
+            if(isset($_POST['submit']))
+        {
+       
+        $q=$_POST['Quiz']; 
+        $p=$_POST['Projects']; 
+        $m_e=$_POST['Midterm_Exam'];
+        $e=$_POST['Exercises']; 
+        $percent1 = 20;
+        $percent2 = 30;
+       
+        $Quiz =  ($percent1 /100) * (float)$q;
+        $Projects =  ($percent2/100)* (float)$p ;
+        $Midterm_Exam = ( $percent2 /100) * (float)$m_e;
+        $Exercises =  ($percent2 /100)* (float)$e;
+        
+        
+        $MidtermGrade=$Quiz+$Projects+$Midterm_Exam+$Exercises ; //total marks
+       
+    
+
+        if($MidtermGrade>=1.0&&$MidtermGrade<=1.5)
+            $grade="Perfect";
+        if($MidtermGrade>=1.6&&$MidtermGrade<=2.0)
+            $grade="Great";
+        if($MidtermGrade>=2.1&&$MidtermGrade<=2.5)
+            $grade="Good";
+        if($MidtermGrade>=2.6&&$MidtermGrade<=3.0)  
+            $grade="Try Harder!";
+        if($MidtermGrade>=3.1)
+            $grade="Better Luck Next Time";
+        echo "<br>";
+      
+        echo "<font size=5><center>Midterm Grade is: ".round($MidtermGrade,PHP_ROUND_HALF_UP )."</center><br>"; 
+        echo "<font size=5><center>Message: ".$grade."</center>"; 
+    }
+    ?>
+                
+        </form>
+        </center>
+
+        
+    </head>
+</html>
